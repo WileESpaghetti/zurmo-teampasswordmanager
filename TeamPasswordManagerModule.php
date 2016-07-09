9cbbb8ae2446091b7fda994b5215246866b848ab
Lehman Black <WileESpaghetti@users.noreply.github.com>
2016-07-09 00:02:24 -0500

    Add configuration link

--- TeamPasswordManagerModule.php
+++ TeamPasswordManagerModule.php
@@ -18,4 +18,21 @@
         {
             return Zurmo::t('TeamPasswordManagerModule', 'Team Password Manager', array(), null, $language);
         }
+        
+        public static function getDefaultMetadata()
+        {
+            $metadata = array();
+            $metadata['global'] = array(
+                'configureMenuItems' => array(
+                    array(
+                        'category'         => ZurmoModule::ADMINISTRATION_CATEGORY_GENERAL,
+                        'titleLabel'       => "eval:Zurmo::t('TeamPasswordManagerModule', 'Team Password Manager Configuration')",
+                        'descriptionLabel' => "eval:Zurmo::t('TeamPasswordManagerModule', 'Manage Team Password Manager Configuration')",
+                        'route'            => '/teamPasswordManager/default/apiConfigurationEdit',
+                        'right'            => ZurmoModule::RIGHT_ACCESS_ADMINISTRATION,
+                    ),
+                ),
+            );
+            return $metadata;
+        }
     }
