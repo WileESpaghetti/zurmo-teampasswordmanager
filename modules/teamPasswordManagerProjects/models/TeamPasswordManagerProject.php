1e51ceb18bbc93ef9460ff93fba000ad36a69e21
Lehman Black <WileESpaghetti@users.noreply.github.com>
2016-07-22 21:37:23 -0500

    Add project model metadata

--- modules/teamPasswordManagerProjects/models/TeamPasswordManagerProject.php
+++ modules/teamPasswordManagerProjects/models/TeamPasswordManagerProject.php
@@ -31,6 +31,35 @@
         {
             $metadata = parent::getDefaultMetadata();
             $metadata[__CLASS__] = array(
+                'members' => array(
+                    'name',
+                    'notes',
+                    'parentId',
+                    'projectId',
+                    'tags' // TODO convert to something akin to product categories
+                ),
+                'relations' => array(
+                ),
+                'derivedRelationsViaCastedUpModel' => array(
+                ),
+                'rules' => array(
+                    array('name',          'required'),
+                    array('name',          'type',    'type' => 'string'),
+                    array('name',          'length',  'min'  => 1, 'max' => 255),
+                    array('tags',          'type',    'type' => 'string'),
+                    array('tags',          'length',  'min'  => 1, 'max' => 255),
+                    array('parentId',      'type',    'type' => 'int'), // FIXME must be >= 0
+                    array('notes',         'type',    'type' => 'string'), // FIXME must be >= 0
+                ),
+                'elements' => array(
+                ),
+                'customFields' => array(
+                ),
+                'defaultSortAttribute' => 'name',
+                'rollupRelations' => array(
+                ),
+                'noAudit' => array(
+                ),
             );
             return $metadata;
         }
